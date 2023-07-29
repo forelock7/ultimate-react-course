ce33ccd26cf463a6c01bd2fa9cf7309c0fe56d42
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-07-29 14:57:32 +0300

    3 - Create more components

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -49,13 +49,36 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!!!!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
 }
+
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString()} We're currently open</footer>;
+}
+
+// function Footer() {
+//   return React.createElement('footer', null, `We are curently open!`);
+// }
 
 function Pizza() {
   return (
