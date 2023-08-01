718e74b58ad09956ae23f9e2cb429c3c72f90f49
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-08-01 10:05:49 +0300

    3 - Conditional rendering with multiple returns

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -94,6 +94,9 @@ function Menu() {
 
 function Pizza(props) {
   console.log(props);
+
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -108,13 +111,19 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 11;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
   // if (hour >=openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry we're closed");
 
+  if (!isOpen)
+    return (
+      <p>
+        We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+      </p>
+    );
   return (
     <footer className="footer">
       {isOpen ? (
