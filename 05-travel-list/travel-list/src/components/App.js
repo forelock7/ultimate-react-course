87aed530dcc361f04b99cc610e94e416c5810401
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-08-09 10:21:00 +0300

    5 - 88. Moving Components Into Separate Files

---
+++ 05-travel-list/travel-list/src/components/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import Logo from './Logo';
+import Form from './Form';
+import PackingList from './PackingList';
+import Stats from './Stats';
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm('Are you sure you want to delete all items');
+    if (confirmed) setItems([]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item)),
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onClearList={handleClearList}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
