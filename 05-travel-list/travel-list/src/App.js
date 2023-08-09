d2654a7d87090a1c9f7cd5a883fe3aa7ab67b7cc
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-08-09 10:07:46 +0300

    5 - Clear list of items

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -11,6 +11,11 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm('Are you sure you want to delete all items');
+    if (confirmed) setItems([]);
+  }
+
   function handleToggleItem(id) {
     setItems((items) =>
       items.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item)),
@@ -21,7 +26,12 @@ export default function App() {
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItem} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onClearList={handleClearList}
+        onToggleItem={handleToggleItem}
+      />
       <Stats items={items} />
     </div>
   );
@@ -69,7 +79,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onClearList, onToggleItem }) {
   const [sortBy, setSortBy] = useState('input');
 
   let sortedItems;
@@ -93,6 +103,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear list</button>
       </div>
     </div>
   );
