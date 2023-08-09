0425a1ffc0f9449fe96984a16afad406ad9ac618
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-08-09 21:08:24 +0300

    4 - children props 2

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -26,9 +26,7 @@ export default function App() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
@@ -42,6 +40,15 @@ export default function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
