3a7890167723f0fc913e06c4157155bc5e496fe6
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-09-22 19:51:46 +0300

    Section 12: Effects and Data Fetching

--- 07-usepopcorn/usepopcorn/src/App_v1.js
+++ 07-usepopcorn/usepopcorn/src/App_v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const tempMovieData = [
   {
@@ -49,19 +49,9 @@ const tempWatchedData = [
 
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = '60c83728';
-
 export default function App() {
   const [watched, setWatched] = useState(tempWatchedData);
   const [movies, setMovies] = useState(tempMovieData);
-
-  useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.Search);
-      });
-  }, []);
 
   return (
     <>
--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from 'react';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+// import { useState } from 'react';
+import './index.css';
+import App from './App';
+// import StarRating from './StarRating';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+    <App />
+    {/* <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
     <StarRating maxRating={5} color="red" className="test" defaultRating={3} />
-    <Test />
+    <Test /> */}
     {/* <StarRating maxRating={10} />
     <StarRating /> */}
   </React.StrictMode>,
