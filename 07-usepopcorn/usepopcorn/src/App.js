5f1b251cdce8723983bf59d22b77df45168c810a
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2024-01-18 10:37:38 +0200

    NOT manipulate DOM directly in React

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -197,6 +197,12 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  useEffect(function () {
+    const el = document.querySelector('.search');
+    console.log(el);
+    el.focus();
+  }, []);
+
   return (
     <input
       className="search"
