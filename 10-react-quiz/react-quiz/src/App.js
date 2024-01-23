388971e2581669926610ee805e39b989b65b29ce
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2024-01-23 10:21:53 +0200

    Fake API and add reducer to quiz project

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -1,7 +1,34 @@
 import Header from './Header';
 import Main from './Main';
+import { useEffect, useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, action: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action is unknown');
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
