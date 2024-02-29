1d633cf8a879789c1b942b4ea28b7e2b6da02156
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2024-02-29 21:32:16 +0200

    210. Building the Pages

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
