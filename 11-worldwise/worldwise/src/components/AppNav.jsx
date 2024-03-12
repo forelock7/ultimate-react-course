010075c2ab7921ca35dd8e24726296d4dd40c837
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2024-03-12 21:54:25 +0200

    212. Nested Routes and Index Route

--- 11-worldwise/worldwise/src/components/AppNav.jsx
+++ 11-worldwise/worldwise/src/components/AppNav.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from 'react-router-dom';
 import { nav } from './AppNav.module.css';
 
 const AppNav = () => {
   return (
-    <nav clas sName={nav}>
-      App navigation
+    <nav className={nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
