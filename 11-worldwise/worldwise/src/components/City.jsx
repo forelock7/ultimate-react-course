02119cb4d45879fab9d161b1be54a553e6739438
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2024-03-15 19:29:41 +0200

    217. Reading and Setting a Query String

--- 11-worldwise/worldwise/src/components/City.jsx
+++ 11-worldwise/worldwise/src/components/City.jsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import styles from './City.module.css';
 
 const formatDate = (date) =>
@@ -11,6 +12,9 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSerchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
 
   // TEMP DATA
   const currentCity = {
@@ -22,7 +26,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <div>
+      <h1>City {id}</h1>{' '}
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+    </div>
+  );
 
   // return (
   //   <div className={styles.city}>
--- 11-worldwise/worldwise/src/components/Map.jsx
+++ 11-worldwise/worldwise/src/components/Map.jsx
@@ -1,6 +1,24 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSerchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSerchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  );
 };
 
 export default Map;
