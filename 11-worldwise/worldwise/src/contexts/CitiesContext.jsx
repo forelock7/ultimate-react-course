973c4df17c2dc9f7451b74b14609bfafac002090
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-11 19:31:37 +0300

    252. Back to The "WorldWise" App

--- 11-worldwise/worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/worldwise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useContext, useReducer } from 'react';
+import { createContext, useEffect, useContext, useReducer, useCallback } from 'react';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -58,17 +58,20 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: 'loading' });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: 'city/loaded', payload: data });
-    } catch {
-      dispatch({ type: 'rejected', payload: 'There was an error loading city...' });
-    }
-  }
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: 'loading' });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: 'city/loaded', payload: data });
+      } catch {
+        dispatch({ type: 'rejected', payload: 'There was an error loading city...' });
+      }
+    },
+    [currentCity.id],
+  );
 
   async function createCity(newCity) {
     dispatch({ type: 'loading' });
