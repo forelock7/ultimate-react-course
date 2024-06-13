e9f84ea34989895d67646ec5cca8ef2010f3a35f
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-13 11:17:48 +0300

    256. CHALLENGE #1: Fix Performance Issues in "Workout Timer" - fix

--- 13-workout-timer/workout-timer/src/Calculator.js
+++ 13-workout-timer/workout-timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { memo, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -90,4 +90,4 @@ function Calculator({ workouts, allowSound }) {
   );
 }
 
-export default Calculator;
+export default memo(Calculator);
