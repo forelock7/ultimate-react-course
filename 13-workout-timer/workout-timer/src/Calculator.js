a569450174425f7175fab505844627f230d02f01
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-17 18:33:17 +0300

    259. Closures in Effects

--- 13-workout-timer/workout-timer/src/Calculator.js
+++ 13-workout-timer/workout-timer/src/Calculator.js
@@ -28,6 +28,10 @@ function Calculator({ workouts, allowSound }) {
     [number, sets, speed, durationBreak],
   );
 
+  useEffect(function() {
+    document.title = `Your ${number}-exercise workout`
+  }, [number])
+
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
