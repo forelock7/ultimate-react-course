ca8c8c9079fa02a8d20368fbe55f766c5d73d851
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-18 20:44:35 +0300

    263. Creating a Redux Store

--- 15-redux-intro/redux-intro/src/store.js
+++ 15-redux-intro/redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -13,10 +15,26 @@ function reducer(state = initialState, action) {
     case 'account/requestLoan':
       if (state.loan > 0) return state;
       // later
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case 'account/payloan':
       return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan };
     default:
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: 'account/withdraw', payload: 200 });
+console.log(store.getState());
+store.dispatch({ type: 'account/requestLoan', payload: { amount: 1000, purpose: 'Buy Car' } });
+console.log(store.getState());
+store.dispatch({ type: 'account/payloan' });
+console.log(store.getState());
