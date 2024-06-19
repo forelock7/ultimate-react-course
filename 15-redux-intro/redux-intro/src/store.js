252b150d12c5de34dbb57fc631dae2d9de713bdc
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-19 10:43:10 +0300

    265. Adding More State: Customer

--- 15-redux-intro/redux-intro/src/store.js
+++ 15-redux-intro/redux-intro/src/store.js
@@ -1,12 +1,37 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createAt: '',
+};
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        reateAt: action.payload.createAt,
+      };
+    case 'customer/updateName':
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
@@ -28,7 +53,9 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({ account: accountReducer, customer: customerReducer });
+
+const store = createStore(rootReducer);
 
 // store.dispatch({ type: 'account/deposit', payload: 500 });
 // console.log(store.getState());
@@ -62,4 +89,18 @@ console.log(store.getState());
 store.dispatch(requestLoan(1000, 'Buy Car'));
 console.log(store.getState());
 store.dispatch(payloan());
+console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalID, createAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: 'account/updateName', payload: fullName };
+}
+
+store.dispatch(createCustomer('Vova', 'ewewew32323'));
 console.log(store.getState());
