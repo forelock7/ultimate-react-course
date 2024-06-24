9279b0cfdcd9286427fefd3dc88569d367ce02e1
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-24 17:49:33 +0300

    277. Creating the Customer Slice

--- 15-redux-intro/redux-intro/src/features/customers/customerSlice.js
+++ 15-redux-intro/redux-intro/src/features/customers/customerSlice.js
@@ -1,38 +1,71 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   fullName: '',
   nationalID: '',
   createAt: '',
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case 'customer/createCustomer':
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        reateAt: action.payload.createAt,
-      };
-    case 'customer/updateName':
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-    default:
-      return state;
-  }
-}
+const customerReducer = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: { fullName, nationalID, createAt: new Date().toISOString() },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createAt = action.payload.createAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: 'customer/createCustomer',
-    payload: { fullName, nationalID, createAt: new Date().toISOString() },
-  };
-}
+export const { createCustomer, updateName } = customerReducer.actions;
+export default customerReducer.reducer;
 
-export function updateName(fullName) {
-  return { type: 'account/updateName', payload: fullName };
-}
+// const initialStateCustomer = {
+//   fullName: '',
+//   nationalID: '',
+//   createAt: '',
+// };
+
+// export default function customerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case 'customer/createCustomer':
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         reateAt: action.payload.createAt,
+//       };
+//     case 'customer/updateName':
+//       return {
+//         ...state,
+//         fullName: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export function createCustomer(fullName, nationalID) {
+//   return {
+//     type: 'customer/createCustomer',
+//     payload: { fullName, nationalID, createAt: new Date().toISOString() },
+//   };
+// }
+
+// export function updateName(fullName) {
+//   return { type: 'account/updateName', payload: fullName };
+// }
 
 // store.dispatch(createCustomer('Vova', 'ewewew32323'));
 // console.log(store.getState());
