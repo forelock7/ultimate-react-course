59e8fec984dd2d70d3d7ed9203c842c2e05d50c9
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-24 15:18:23 +0300

    271. Making an API Call With Redux Thunks

--- 15-redux-intro/redux-intro/src/store.js
+++ 15-redux-intro/redux-intro/src/store.js
@@ -1,9 +1,10 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
 
 const rootReducer = combineReducers({ account: accountReducer, customer: customerReducer });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
