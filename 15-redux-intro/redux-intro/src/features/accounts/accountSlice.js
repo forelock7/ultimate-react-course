b9ebdc2922d459ccd794ad23a1981fd4052ba56d
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-24 17:22:50 +0300

    276. Back to Thunks

--- 15-redux-intro/redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/redux-intro/src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -35,12 +36,27 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = '';
     },
+    convertingCurrency(state, action) {
+      state.isLoading = true;
+    },
   },
 });
 
-console.log(accountSlice);
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+  return async function (dispatch, getState) {
+    dispatch({ type: 'account/convertingCurrency' });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    console.log(converted);
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
+}
 
 export default accountSlice.reducer;
 
