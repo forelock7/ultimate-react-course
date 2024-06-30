f9288915b5f032d744902142137d1b80a1926f52
Volodymyr <volodymyr.chubenko@sisense.com>
2024-06-30 21:04:12 +0300

    297. Styling Text

--- 16-fast-react-pizza/fast-react-pizza/src/features/cart/CartOverview.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div class="bg-stone-800 text-stone-200">
-      <p class="text-stone-300">
+    <div class="bg-stone-800 uppercase text-stone-200">
+      <p class="font-semibold text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
---
+++ 16-fast-react-pizza/fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+const Username = () => {
+  return <div className="text-sm font-semibold">Jonas</div>;
+};
+
+export default Username;
