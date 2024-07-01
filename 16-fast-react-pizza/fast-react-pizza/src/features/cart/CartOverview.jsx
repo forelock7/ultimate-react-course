d6515849da2339adc0e4b908f446f744069a0d9d
Volodymyr <volodymyr.chubenko@sisense.com>
2024-07-01 20:32:50 +0300

    298. The Box Model: Spacing, Borders, and Display

--- 16-fast-react-pizza/fast-react-pizza/src/features/cart/CartOverview.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div class="bg-stone-800 uppercase text-stone-200">
-      <p class="font-semibold text-stone-300">
+    <div class="bg-stone-800 p-4 uppercase text-stone-200">
+      <p class="space-x-4 font-semibold text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- 16-fast-react-pizza/fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 const Username = () => {
-  return <div className="text-sm font-semibold">Jonas</div>;
+  return <div className="hidden text-sm font-semibold">Jonas</div>;
 };
 
 export default Username;
