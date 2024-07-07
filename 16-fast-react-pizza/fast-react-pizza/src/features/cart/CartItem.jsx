70fcf6ca4914a14382115139386b0a3f556136c5
Volodymyr <volodymyr.chubenko@sisense.com>
2024-07-07 16:25:38 +0300

    320. Updating Cart Quantities

--- 16-fast-react-pizza/fast-react-pizza/src/features/cart/CartItem.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/cart/CartItem.jsx
@@ -1,8 +1,12 @@
 import { formatCurrency } from '../../utils/helpers';
+import { useSelector } from 'react-redux';
 import DeleteItem from './DeleteItem';
+import UpdateItemQuantity from './UpdateItemQuantity';
+import { getCurrentQuantityById } from '../cart/cartSlice';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -11,6 +15,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <UpdateItemQuantity pizzaId={pizzaId} currentQuantity={currentQuantity} />
         <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
--- 16-fast-react-pizza/fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import { formatCurrency } from '../../utils/helpers';
 import Button from '../../ui/Button';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -34,7 +35,12 @@ function MenuItem({ pizza }) {
             <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity pizzaId={id} currentQuantity={currentQuantity} />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
           {!soldOut && !isInCart && (
             <Button onClick={handleAddToCart} type="small">
               Add to cart
