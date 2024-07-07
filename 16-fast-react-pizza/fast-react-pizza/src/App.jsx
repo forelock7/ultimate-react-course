7e170fa78f344d08100b3fbdd1f9672285c120d7
Volodymyr <volodymyr.chubenko@sisense.com>
2024-07-07 19:15:47 +0300

    325. Updating Data Without Navigation

--- 16-fast-react-pizza/fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/App.jsx
@@ -4,6 +4,7 @@ import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 
@@ -36,6 +37,7 @@ const router = createBrowserRouter([
         element: <Order />,
         errorElement: <Error />,
         loader: orderLoader,
+        action: updateOrderAction,
       },
     ],
   },
--- 16-fast-react-pizza/fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/Order.jsx
@@ -5,6 +5,7 @@ import { calcMinutesLeft, formatCurrency, formatDate } from '../../utils/helpers
 import { getOrder } from '../../services/apiRestaurant';
 import { useFetcher, useLoaderData } from 'react-router-dom';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   const order = useLoaderData();
@@ -74,6 +75,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
