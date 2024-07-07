da2bc282f2fdcebfde14193d98a2933be3a680c0
Volodymyr <volodymyr.chubenko@sisense.com>
2024-07-07 14:04:01 +0300

    314. Reading and Updating the User State

--- 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -32,6 +33,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
@@ -48,7 +50,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            defaultValue={username}
+            name="customer"
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
@@ -57,7 +65,9 @@ function CreateOrder() {
             <input className="input w-full" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && (
-            <p className="mt-2 rounded-md text-xs text-red-700 bg-red-100 p-2">{formErrors.phone}</p>
+            <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+              {formErrors.phone}
+            </p>
           )}
         </div>
 
