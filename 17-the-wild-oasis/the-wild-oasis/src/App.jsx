77ffa0d2c7b5cc52621f8749278606d9c7080184
Volodymyr <volodymyr.chubenko@sisense.com>
2024-07-08 23:20:03 +0300

    330. Global Styles With Styled Components

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -6,38 +9,23 @@ const H1 = styled.h1`
   background-color: yellow;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
-`
+`;
 
 const App = () => {
   return (
-    <StyledApp>
-      <H1>The Wiled Oasis</H1>
-      <Button onClick={() => alert('Check in')}>Check in</Button>
-      <Button onClick={() => alert('Check out')}>Check out</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wiled Oasis</H1>
+        <Button onClick={() => alert('Check in')}>Check in</Button>
+        <Button onClick={() => alert('Check out')}>Check out</Button>
 
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 };
 
--- 17-the-wild-oasis/the-wild-oasis/src/styles/GlobalStyles.js
+++ 17-the-wild-oasis/the-wild-oasis/src/styles/GlobalStyles.js
@@ -1,18 +1,6 @@
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
+import { createGlobalStyle } from 'styled-components';
 
-/* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
-
+const GlobalStyles = createGlobalStyle`
 :root {
   /* Indigo */
   --color-brand-50: #eef2ff;
@@ -154,6 +142,9 @@ img {
   /* For dark mode */
   filter: grayscale(var(--image-grayscale)) opacity(var(--image-opacity));
 }
+`;
+
+export default GlobalStyles;
 
 /*
 FOR DARK MODE
