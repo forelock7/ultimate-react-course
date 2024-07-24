c1aa3920240b40b28c9f5d0cf87d21754659929b
Volodymyr <volodymyr.chubenko@sisense.com>
2024-07-24 09:40:52 +0300

    352. Introducing Another Library: React Hook Form

--- 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import CabinTable from '../features/cabins/CabinTable';
+import Button from '../ui/Button';
+import { useState } from 'react';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
 
 function Cabins() {
+  const [showForm, setShowForm] = useState();
+
   return (
     <>
       <Row type="horizontal">
@@ -11,6 +16,8 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
