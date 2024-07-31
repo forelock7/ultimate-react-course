dd02667bc1d6a63ab2b4511426420cf8ee829317
Volodymyr <volodymyr.chubenko@sisense.com>
2024-07-31 09:50:05 +0300

    370. Confirming Cabin Deletions

--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -84,12 +84,13 @@ const CabinRow = ({ cabin }) => {
           <Modal.Window name="edit">
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
-          <Modal.Open>
+
+          <Modal.Open opens="delete">
             <button>
               <HiTrash />
             </button>
           </Modal.Open>
-          <Modal.Window>
+          <Modal.Window name="delete">
             <ConfirmDelete
               resourceName="cabins"
               disabled={isDeleting}
