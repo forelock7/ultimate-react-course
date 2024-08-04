9838a4fbd8359896e59030031693232335d12353
Volodymyr <volodymyr.chubenko@sisense.com>
2024-08-04 19:59:12 +0300

    380. API-Side Sorting: Sorting Bookings

--- 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
@@ -10,13 +10,18 @@ export function useBookings() {
   const filter =
     !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
 
+  // SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
--- 17-the-wild-oasis/the-wild-oasis/src/services/apiBookings.js
+++ 17-the-wild-oasis/the-wild-oasis/src/services/apiBookings.js
@@ -10,8 +10,11 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   // FILTER
-  if (filter !== null) query = query.eq(filter.field, filter.value);
+  if (filter) query = query.eq(filter.field, filter.value);
   // if (filter !== null) query = query[filter.method](filter.field, filter.value);
+
+  // SORT
+  if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' });
 
   const { data, error } = await query;
 
