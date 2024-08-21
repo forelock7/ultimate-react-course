49309dbe4c72d0e7f0e814d865c940abf70eddfd
Volodymyr <volodymyr.chubenko@sisense.com>
2024-08-21 10:34:09 +0300

    390. Authorization: Protecting Routes

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -14,6 +14,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
 import Booking from './pages/Booking';
 import CheckinBooking from './features/check-in-out/CheckinBooking';
+import ProtectedRoute from './ui/ProtectedRoute';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +32,13 @@ const App = () => {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
--- 17-the-wild-oasis/the-wild-oasis/src/services/apiAuth.js
+++ 17-the-wild-oasis/the-wild-oasis/src/services/apiAuth.js
@@ -9,3 +9,15 @@ export async function login({ email, password }) {
   if (error) throw new Error(error.message);
   return data;
 }
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
