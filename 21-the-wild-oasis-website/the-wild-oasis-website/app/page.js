4475ebf8db35ef117da473dc7ee34dd7531480a2
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2025-01-22 21:17:46 +0200

    423. Navigating Between Pages

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/page.js
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>The Wild Oasis. Welcome to paradise.</h1>;
+import Link from 'next/link';
+import Navigation from './components/Navigation';
+
+export default function Page() {
+  return (
+    <div>
+      <Navigation />
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
