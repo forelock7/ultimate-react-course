4b2d261496398753ff585edc26dc6f32f8acf4ba
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2025-02-05 10:30:13 +0200

    435. Adding Page Metadata and Favicon

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -1,5 +1,9 @@
 import Counter from '@/app/_components/Counter';
 
+export const metadata = {
+  title: 'Cabins',
+};
+
 export default function Page() {
   return (
     <div>
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/layout.js
@@ -4,7 +4,12 @@ import Navigation from './_components/Navigation';
 import '@/app/_styles/globals.css';
 
 export const metadata = {
-  title: 'The Wild Oasis',
+  title: {
+    template: '%s / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests',
 };
 
 export default function RootLayout({ children }) {
