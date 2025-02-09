692a9ba09aba047d2f047bf8cc344ef11ee3dcfc
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2025-02-09 19:25:46 +0200

    439. Building the Home Page

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
         <div className="flex-1 px-8 py-12">
