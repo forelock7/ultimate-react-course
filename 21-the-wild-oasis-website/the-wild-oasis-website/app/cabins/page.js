5faab8000c9180f72a0b75210bcd363ae7753c90
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2025-03-21 18:14:08 +0200

    445. Fetching and Displaying Cabin List

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -1,13 +1,13 @@
-import Counter from '@/app/_components/Counter';
 import CabinCard from '@/app/_components/CabinCard';
+import { getCabins } from '@/app/_lib/data-service';
 
 export const metadata = {
   title: 'Cabins',
 };
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
 
   return (
     <div>
