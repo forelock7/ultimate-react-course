3705210e627857ea5e8cb1591743f90c2c0fcf40
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2025-03-23 21:06:59 +0200

    448. Streaming UI With Suspense: Cabin List

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -1,14 +1,12 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabins } from '@/app/_lib/data-service';
+import { Suspense } from 'react';
+import CabinList from '../_components/CabinList';
+import Spinner from '../_components/Spinner';
 
 export const metadata = {
   title: 'Cabins',
 };
 
 export default async function Page() {
-  // CHANGE
-  const cabins = await getCabins();
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
@@ -20,13 +18,9 @@ export default async function Page() {
         paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
