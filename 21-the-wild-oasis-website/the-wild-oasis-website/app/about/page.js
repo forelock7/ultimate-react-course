bcd26f192118b237b2601f26c8532be7f1ca9961
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2025-03-24 19:49:51 +0200

    454. Analyzing Rendering in Our App

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/about/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/about/page.js
@@ -14,18 +14,18 @@ export default function Page() {
 
         <div className="space-y-8">
           <p>
-            Where nature's beauty and comfortable living blend seamlessly. Hidden away in the heart
-            of the Italian Dolomites, this is your paradise away from home. But it's not just about
-            the luxury cabins. It's about the experience of reconnecting with nature and enjoying
+            Where nature&apos;s beauty and comfortable living blend seamlessly. Hidden away in the heart
+            of the Italian Dolomites, this is your paradise away from home. But it&apos;s not just about
+            the luxury cabins. It&apos;s about the experience of reconnecting with nature and enjoying
             simple pleasures with family.
           </p>
           <p>
-            Our 8 luxury cabins provide a cozy base, but the real freedom and peace you'll find in
+            Our 8 luxury cabins provide a cozy base, but the real freedom and peace you&apos;ll find in
             the surrounding mountains. Wander through lush forests, breathe in the fresh air, and
             watch the stars twinkle above from the warmth of a campfire or your hot tub.
           </p>
           <p>
-            This is where memorable moments are made, surrounded by nature's splendor. It's a place
+            This is where memorable moments are made, surrounded by nature&apos;s splendor. It&apos;s a place
             to slow down, relax, and feel the joy of being together in a beautiful setting.
           </p>
         </div>
@@ -61,9 +61,9 @@ export default function Page() {
             family as a testament to our dedication to creating a warm, welcoming environment.
           </p>
           <p>
-            Over the years, we've maintained the essence of The Wild Oasis, blending the timeless
+            Over the years, we&apos;ve maintained the essence of The Wild Oasis, blending the timeless
             beauty of the mountains with the personal touch only a family business can offer. Here,
-            you're not just a guest; you're part of our extended family. So join us at The Wild
+            you&apos;re not just a guest; you&apos;re part of our extended family. So join us at The Wild
             Oasis soon, where tradition meets tranquility, and every visit is like coming home.
           </p>
 
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -13,7 +13,7 @@ export default async function Page() {
       <p className="text-primary-200 text-lg mb-10">
         Cozy yet luxurious cabins, located right in the heart of the Italian Dolomites. Imagine
         waking up to beautiful mountain views, spending your days exploring the dark forests around,
-        or just relaxing in your private hot tub under the stars. Enjoy nature's beauty in your own
+        or just relaxing in your private hot tub under the stars. Enjoy nature&apos;s beauty in your own
         little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to
         paradise.
       </p>
