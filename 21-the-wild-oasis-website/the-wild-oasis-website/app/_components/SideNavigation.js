b85820f8a2220a358d2efefe294c76ddf03528f0
Volodymyr Chubenko <forelock7@gmail.com>
2025-05-05 19:21:31 +0300

    464. Highlighting Current Side Navigation Link

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/SideNavigation.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/SideNavigation.js
@@ -1,6 +1,9 @@
+'use client';
+
 import { CalendarDaysIcon, HomeIcon, UserIcon } from '@heroicons/react/24/solid';
 import SignOutButton from './SignOutButton';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const navLinks = [
   {
@@ -21,13 +24,17 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathname === link.href ? 'bg-primary-900' : ''
+              }`}
               href={link.href}
             >
               {link.icon}
