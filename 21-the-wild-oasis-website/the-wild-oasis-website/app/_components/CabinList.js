28ed9e93b8c7ad949f185b1c9d92f55043e66218
Volodymyr Chubenko <forelock7@gmail.com>
2025-05-06 19:54:07 +0300

    465. Sharing State Between Client and Server: The URL

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/CabinList.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/CabinList.js
@@ -2,15 +2,22 @@
 import CabinCard from '@/app/_components/CabinCard';
 import { getCabins } from '@/app/_lib/data-service';
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // noStore();
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  let dispayedCabins;
+  if (filter === 'all') dispayedCabins = cabins;
+  if (filter === 'small') dispayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  if (filter === 'medium')
+    dispayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7);
+  if (filter === 'large') dispayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {dispayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
+import Filter from '../_components/Filter';
 
 export const revalidate = 3600;
 
@@ -8,20 +9,24 @@ export const metadata = {
   title: 'Cabins',
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  console.log(searchParams);
+  const filter = searchParams?.capacity ?? 'all';
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
       <p className="text-primary-200 text-lg mb-10">
         Cozy yet luxurious cabins, located right in the heart of the Italian Dolomites. Imagine
         waking up to beautiful mountain views, spending your days exploring the dark forests around,
-        or just relaxing in your private hot tub under the stars. Enjoy nature&apos;s beauty in your own
-        little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to
+        or just relaxing in your private hot tub under the stars. Enjoy nature&apos;s beauty in your
+        own little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to
         paradise.
       </p>
-
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
