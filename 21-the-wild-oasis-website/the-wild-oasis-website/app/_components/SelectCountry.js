769e136187dc3169c424fadac1271739e8e9d7c6
Volodymyr Chubenko <forelock7@gmail.com>
2025-05-11 18:26:25 +0300

    466. Advanced: Server Components in Client Components

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/SelectCountry.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/SelectCountry.js
@@ -3,8 +3,8 @@ import { getCountries } from '@/app/_lib/data-service';
 // Let's imagine your colleague already built this component 😃
 
 async function SelectCountry({ defaultCountry, name, id, className }) {
-  const countries = [];
-  // const countries = await getCountries();
+  // const countries = [];
+  const countries = await getCountries();
   const flag = countries.find((country) => country.name === defaultCountry)?.flag ?? '';
 
   return (
