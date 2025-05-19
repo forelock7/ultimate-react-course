b9b30c734a6486e3583c989a8c83c04171202073
Volodymyr Chubenko <forelock7@gmail.com>
2025-05-19 20:36:58 +0300

    475. Building a Custom Sign In Page

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/account/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/account/page.js
@@ -1,7 +1,12 @@
+import { auth } from '../_lib/auth';
+
 export const metadata = {
   title: 'Guest area',
 };
 
-export default function Page() {
-  return <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, Jonas</h2>;
+export default async function Page() {
+  const session = await auth();
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, {session.user.name}</h2>
+  );
 }
