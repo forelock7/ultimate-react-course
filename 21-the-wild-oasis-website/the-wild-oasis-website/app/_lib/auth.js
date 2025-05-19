005b2897e912baf048481e87b68cb0ebca6016c4
Volodymyr Chubenko <forelock7@gmail.com>
2025-05-19 20:16:09 +0300

    474. Protecting Routes With NextAuth Middleware

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/auth.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/auth.js
@@ -5,6 +5,11 @@ const authConfig = {
   providers: [
     Google({ clientId: process.env.AUTH_GOOGLE_ID, clientSecret: process.env.AUTH_GOOGLE_SECRET }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
