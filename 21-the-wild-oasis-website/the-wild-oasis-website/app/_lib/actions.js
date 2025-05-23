ac28e88ad451b7e998fb2bc85482b677f8958195
Volodymyr Chubenko <forelock7@gmail.com>
2025-05-23 10:54:21 +0300

    481. Manual Cache Revalidation

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
 
@@ -21,6 +22,8 @@ export async function updateGuest(formData) {
     .eq('id', (await session).user.guestId);
 
   if (error) throw new Error('Guest could not be updated');
+
+  revalidatePath('/account/profile');
 }
 
 export async function signInAction() {
