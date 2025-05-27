545054205919935784fb18464d9e0ee7b77bc06b
Volodymyr Chubenko <forelock7@gmail.com>
2025-05-27 11:34:12 +0300

    484. Deleting a Reservation

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
+import { getBookings } from './data-service';
 
 export async function updateGuest(formData) {
   const session = auth();
@@ -24,6 +25,23 @@ export async function updateGuest(formData) {
   if (error) throw new Error('Guest could not be updated');
 
   revalidatePath('/account/profile');
+}
+
+export async function deleteReservetion(bookingId) {
+  const session = auth();
+  if (!session) throw new Error('You must be logged in');
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error('You are not allowed to delete this booking');
+
+  const { error } = await supabase.from('bookings').delete().eq('id', bookingId);
+
+  if (error) throw new Error('Booking could not be deleted');
+
+  revalidatePath('/account/reservations');
 }
 
 export async function signInAction() {
