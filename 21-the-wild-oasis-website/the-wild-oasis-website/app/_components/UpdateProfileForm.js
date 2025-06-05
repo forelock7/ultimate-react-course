a605a5e5e4b3d95c3975c5ff899848f197862971
Volodymyr Chubenko <forelock7@gmail.com>
2025-06-05 19:06:08 +0300

    486. CHALLENGE #1: Updating a Reservation

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -54,16 +54,9 @@ function UpdateProfileForm({ guest, children }) {
   );
 }
 
-function Button() {
+function Button({ children }) {
   const { pending } = useFormStatus();
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? 'Updating...' : 'Update profile'}
-    </button>
-  );
+  return <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>;
 }
 
 export default UpdateProfileForm;
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
 import { getBookings } from './data-service';
+import { redirect } from 'next/navigation';
 
 export async function updateGuest(formData) {
   const session = auth();
@@ -42,6 +43,49 @@ export async function deleteReservetion(bookingId) {
   if (error) throw new Error('Booking could not be deleted');
 
   revalidatePath('/account/reservations');
+}
+
+export async function updateBooking(formData) {
+  console.log(formData);
+  const bookingId = Number(formData.get('bookingId'));
+
+  // 1) Authentication
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+
+  // 2) Authorization
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error('You are not allowed to update this booking');
+
+  // 3) Building update data
+  const updateData = {
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData.get('observations').slice(0, 1000),
+  };
+
+  // 4) Mutation
+  const { error } = await supabase
+    .from('bookings')
+    .update(updateData)
+    .eq('id', bookingId)
+    .select()
+    .single();
+
+  // 5) Error handling
+  if (error) {
+    console.error(error);
+    throw new Error('Booking could not be updated');
+  }
+
+  // 6) Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath('/account/reservations');
+
+  // 7) Redirecting
+  redirect('/account/reservations');
 }
 
 export async function signInAction() {
